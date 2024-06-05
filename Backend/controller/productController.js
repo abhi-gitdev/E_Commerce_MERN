8887e217e5af2e7a3de8091cb06efa97ba8df998
Krishna Magar <magarkrishna2564@gmail.com>
2024-06-05 18:47:44 +0530

    setting product routes

---
+++ Backend/controller/productController.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+
+exports.getAllProducts = asyncHandler(async (req, res) => {
+  res.status(200).send({ message: 'getting all products' })
+})
+
+exports.getProduct = asyncHandler(async (req, res) => {
+  const id = req.params.id
+  res.status(200).send({ message: `getting product ${id}` })
+})
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 
-const port = process.env.PORT || 8001
+const port = process.env.PORT || 8000
 
 const app = express()
 
+app.use('/api/products', require('./routes/productRoutes'))
+
 app.listen(port, () => {
-  console.log(`Running on https://localhost:${port}`)
+  console.log(`Running on http://localhost:${port}`)
 })
