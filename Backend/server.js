d3caebb7a742997cea2c636d9cc30fa37a74d0a7
Krishna Magar <magarkrishna2564@gmail.com>
2024-06-05 19:24:55 +0530

    connecting db

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const connectDB = require('./config/connectDB')
 const port = process.env.PORT || 8000
 
+connectDB()
 const app = express()
 
 app.use('/api/products', require('./routes/productRoutes'))
