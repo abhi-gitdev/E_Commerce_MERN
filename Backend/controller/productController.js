9dc61f40b2fe38401ef38762e9404193d4893d76
Krishna Magar <magarkrishna2564@gmail.com>
2024-06-06 18:36:49 +0530

    created product model

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
+const Product = require('../models/productModel')
 
 exports.getAllProducts = asyncHandler(async (req, res) => {
   res.status(200).send({ message: 'getting all products' })
---
+++ Backend/models/productModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter the name of product'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter description of product'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please enter the price of product'],
+      maxLength: [5, 'Price cannot exceed 5 char'],
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Please enter the available quantity of product'],
+      default: 1,
+    },
+    images: [
+      {
+        public_id: { type: String, required: true },
+        url: { type: String, required: true },
+      },
+    ],
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Product', productSchema)
