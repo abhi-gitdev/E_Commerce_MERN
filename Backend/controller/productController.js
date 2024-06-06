cbee4bd6862fe2ee074088ca6e4e2e0f026e3f81
Krishna Magar <magarkrishna2564@gmail.com>
2024-06-06 19:13:50 +0530

    added update product feature

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
+const mongoose = require('mongoose')
 
 exports.getAllProducts = asyncHandler(async (req, res) => {
   res.status(200).send({ message: 'getting all products' })
@@ -22,6 +23,15 @@ exports.createProduct = asyncHandler(async (req, res) => {
 
 exports.updateProduct = asyncHandler(async (req, res) => {
   const id = req.params.id
-  const { name, description, price, quantity, images } = req.body
-  const product = await Product.find({ _id: id })
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: 'Invalid product ID format' })
+  }
+  const product = await Product.findById(id)
+  if (!product) {
+    return res.status(404).send({ message: 'No product found' })
+  }
+  const newProduct = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+  })
+  res.status(200).send({ newProduct })
 })
