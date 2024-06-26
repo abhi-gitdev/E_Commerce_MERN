e339997e947a9ba0415c7304b533797caaa3e59a
Krishna Magar <magarkrishna2564@gmail.com>
2024-06-27 01:11:08 +0530

    added navbar code and css

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,20 +1,34 @@
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
+import { IoIosCart } from 'react-icons/io'
+import { CgProfile } from 'react-icons/cg'
 
 const Navbar = () => {
   return (
     <>
       <nav>
-        <Link to={'/'}>StreetStyleSprint</Link>
+        <Link to={'/'} className="link">
+          StreetStyleSprint
+        </Link>
         <div class="navDiv" id="pageLink">
-          <Link to={'/'}>Home</Link>
-          <Link to={'/'}>Menu</Link>
-          <Link to={'/'}>About</Link>
+          <Link to={'/'} className="link">
+            Home
+          </Link>
+          <Link to={'/'} className="link">
+            Catalog
+          </Link>
+          <Link to={'/'} className="link">
+            About
+          </Link>
         </div>
         <div class="navDiv">
-          <Link to={'/'}>Cart</Link>
-          <Link to={'/'}>Profile</Link>
+          <Link to={'/'} className="link">
+            <IoIosCart style={{ fontSize: '2rem' }} />
+          </Link>
+          <Link to={'/'} className="link">
+            <CgProfile style={{ fontSize: '2rem' }} />
+          </Link>
         </div>
       </nav>
     </>
